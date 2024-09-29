19420334fb7d3ad15cfe2ba1dff1c3b6db055c50
luqingyang <luqingyang@mengxiang.com>
2024-09-29 19:56:21 +0800

    feat: 修改

---
+++ webpack/rollup/bundle.js
@@ -0,0 +1,9 @@
+const PI = 3.14;
+
+function addPi(x) {
+  return x + PI;
+}
+
+// main.js
+
+console.log(addPi(10));
---
+++ webpack/rollup/main.js
@@ -0,0 +1,4 @@
+// main.js
+import { addPi } from './add.js';
+
+console.log(addPi(10));
