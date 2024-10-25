b4e77eef3a952f7813059d03e446ecd27acb22f2
luqingyang <luqingyang@mengxiang.com>
2024-10-25 17:22:58 +0800

    fix

---
+++ 微前端/qiankun/vue2-app/vue.config.js
@@ -0,0 +1,16 @@
+const { name } = require('./package.json')
+module.exports = {
+    devServer: {
+        port: 81,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      },
+    configureWebpack: {
+        output: {
+            library: `${name}-[name]`,
+            libraryTarget: 'umd',
+            jsonpFunction: `webpackJsonp_${name}`
+        }
+    }
+}
