95c956342e1b27f8e6c919c9d78e4a0dd84b58d1
tringyoung@163.com <trinyoung>
2024-10-29 00:30:44 +0800

    feat: 修改

--- 微前端/qiankun-demo/main-app/src/App.tsx
+++ 微前端/qiankun-demo/main-app/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 // import HomePage from '@/pages/homePage'
 
-import HomePage from './pages/homePage'
+import HomePage from '@/pages/homePage'
 function App() {
   return (
     <div className="App">
